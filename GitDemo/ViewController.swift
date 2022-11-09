f40093f5a110cef61a582d5c7e4178eb1476f7f6
key.cpp <key.cpp@gmail.com>
2022-11-10 00:14:29 +0300

    1 commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Евгений Карась on 7.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Another print statement")
+    }
+
+
+}
+
